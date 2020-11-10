9ea7059d1da2d2c8b0fcf69421ce3bfd0419da37
santiagomtz12 <54302094+santiagomtz12@users.noreply.github.com>
2020-11-10 02:55:43 -0600

    mi primer commit

---
+++ src/poop5/fecha.java
@@ -0,0 +1,80 @@
+
+package poop5;
+/**
+ * Clase Fecha
+ * @author santi
+ */
+
+class fecha {
+    private int dia;
+    private int mes;
+    private int anio;
+
+    public fecha() {
+    }
+    /**
+     * Constructor fecha
+     * @param dia de nacimiento tipo entero
+     * @param mes tipo entero
+     * @param anio tipo entero
+     */
+
+    public fecha(int dia, int mes, int anio) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+    /**
+     * get Dia 
+     * @return el dia de nacimiento
+     */
+    public int getDia() {
+        return dia;
+    }
+    /**
+     * set Dia
+     * @param dia 
+     */
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+    /**
+     * get Mes
+     * @return el mes de nacimiento tipo entero 
+     */
+    public int getMes() {
+        return mes;
+    }
+    /**
+     * set Mes 
+     * @param mes 
+     */
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+    /**
+     * get Año
+     * @return año de nacimiento tipo entero
+     */
+    public int getAnio() {
+        return anio;
+    }
+    /**
+     * set año
+     * @param anio 
+     */
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    /**
+     * Metodo toString 
+     * @return tdos nuestros datos tipo enteros
+     */
+    @Override
+    public String toString() {
+        return "fecha{" + "dia=" + dia + ", mes=" + mes + ", a\u00f1o=" + anio + '}';
+    }
+    
+    
+}
